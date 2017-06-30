c7248b3f39e2671307621721f026583cf1db22fc
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2017-06-30 19:41:58 +0300

    Added plot for combined slip (basic method)

---
+++ tyre-model/combined-slip-basic.py
@@ -0,0 +1,36 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
+def coefficient(slipValue, extremumValue, extremumSlip, asymptoteValue, asymptoteSlip):
+    coefficient = asymptoteValue;
+    absoluteSlip = abs(slipValue);
+    if (absoluteSlip <= extremumSlip):
+        coefficient = (extremumValue / extremumSlip) * absoluteSlip;
+    elif (absoluteSlip > extremumSlip and absoluteSlip < asymptoteSlip):
+        coefficient = ((asymptoteValue - extremumValue) / (asymptoteSlip - extremumSlip)) \
+                              * (absoluteSlip - extremumSlip) + extremumValue;
+    return coefficient
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+X = np.arange(0, 0.4, 0.01)
+Y = np.arange(0, 40, 1)
+X, Y = np.meshgrid(X, Y)
+longfunc = np.vectorize(lambda t: coefficient(t, 1, 0.2, 0.75, 0.4))
+lateralfunc = np.vectorize(lambda t: coefficient(t, 1.0, 20.0, 0.75, 40))
+Z = np.sqrt(longfunc(X)**2 + lateralfunc(Y)**2)
+
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+ax.set_zlim(0.0, 2.0)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
